9c68e297df2432f2d8982e37c0c2c5c4cb327da5
-v <julio.pachon@sydney.edu.au>
2024-08-21 15:12:42 +1000

    adding disclaimer

--- ui.R
+++ ui.R
@@ -2,6 +2,15 @@
 
 ui <- fluidPage(
     titlePanel("Global Soil Regulation Dashboard"),
+
+   
+    
+
+    HTML("<p><strong>Disclaimer:</strong> The data presented reflect only the number of documents analyzed on FAO SoilLEx at August 8, 2024 and should not be used to assess the effectiveness of regional soil governance. Higher document counts do not necessarily indicate superior soil protection or governance.</p>"),
+    
+    HTML(paste(
+        "<p><strong>Citation:</strong> Bourhis, H., Rodriguez Eugenio, N., Poch, R.M., Lefèvre, C., Vargas, R. (2022). SoiLEX, The New Tool of the Global Soil Partnership to Strengthen Soil Governance. In: Ginzky, H., et al. <em>International Yearbook of Soil Law and Policy 2020/2021</em>. International Yearbook of Soil Law and Policy, vol 2020. Springer, Cham. <a href='https://doi.org/10.1007/978-3-030-96347-7_18' target='_blank'>https://doi.org/10.1007/978-3-030-96347-7_18</a></p>"
+    )),
     
     sidebarLayout(
         sidebarPanel(
